f32b16c190c04d2e79bd981e8fb9db7451f9b886
komildeveloper <komildeveloper111@gmail.com>
2021-08-02 16:13:21 +0500

    reset command added

--- config.def.h
+++ config.def.h
@@ -163,6 +163,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY,                       XK_F2,      spawn,  SHCMD("screenkey -s small --scr 2 -p fixed -g 400x100+2150+1330 --opacity .6 --font-color white") },
 	{ MODKEY,                       XK_F3,      spawn,  SHCMD("killall screenkey") },
+	{ MODKEY,                       XK_F5,      spawn,  SHCMD("reset") },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	/* { MODKEY,                       XK_0,      view,           {.ui = ~0 } }, */
 	/* { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, */
