c40be97a84b4e2ba678e3c98c33dcbc43e3a640e
komildeveloper <komildeveloper111@gmail.com>
2022-03-12 16:54:02 +0500

    terminal changed

--- config.def.h
+++ config.def.h
@@ -15,7 +15,12 @@ static const unsigned int gappov    = 23;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro Regular:size=13", "JoyPixels:pixelsize=14:antialias=true:autohint=true" };
+static const char *fonts[]     = {"SauceCodePro Nerd Font Mono:weight=bold:size=12:antialias=true:hinting=true",
+	                              "Mononoki:size=12:antialias=true:autohint=true",
+                                  "Hack:size=11:antialias=true:autohint=true",
+                                  "JoyPixels:size=13:antialias=true:autohint=true"
+						     	};
+
 static const char *upvol[]          = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%",     NULL };
 static const char *downvol[]        = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%",     NULL };
 static const char *mutevol[]        = { "/usr/bin/pactl", "set-sink-mute",   "@DEFAULT_SINK@", "toggle",  NULL };
@@ -101,7 +106,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 #include <X11/XF86keysym.h>
 #include "./patches/shiftview.c"
