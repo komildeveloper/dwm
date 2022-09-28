8a74dceef28e46e92246f55b6e29878600dd0405
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-09-29 00:03:08 +0500

    config.def.h updated

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 2;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 8;   /* systray spacing */
